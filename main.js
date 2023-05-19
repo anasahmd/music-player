b78c55b7974655f27f5f5741206ac946005bd9e8
anasahmd <anasahmad0239@gmail.com>
2023-05-20 02:48:30 +0530

    Add style

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import songs from './songs.js';
 
+const coverImage = document.querySelector('.cover-image');
+const songTitle = document.querySelector('.song-title');
+const songArtist = document.querySelector('.song-artist');
+const btnNext = document.querySelector('.btn-next');
+const btnPrev = document.querySelector('.btn-prev');
+const btnPlay = document.querySelector('.btn-play');
+
 // for (let song of songs) {
 // 	if (song.cover) {
 // 		var blob = new Blob([new Uint8Array(song.cover.data)], {
@@ -11,3 +18,21 @@ import songs from './songs.js';
 // 		img.setAttribute('src', url);
 // 	}
 // }
+
+var blob = new Blob([new Uint8Array(songs[1].cover.data)], {
+	type: songs[0].cover.format,
+});
+var url = URL.createObjectURL(blob);
+const img = document.createElement('img');
+coverImage.appendChild(img);
+img.setAttribute('src', url);
+
+songTitle.innerHTML = songs[1].title;
+songArtist.innerHTML = songs[1].artist;
+
+btnNext.addEventListener('click', () => {});
+
+function musicPlayer() {
+	let playing = false;
+	let currentTrack = 0;
+}
