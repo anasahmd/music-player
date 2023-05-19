71784fce1a2d9b6e16ec3b48df8a6b87495a4025
anasahmd <anasahmad0239@gmail.com>
2023-05-20 00:57:10 +0530

    Got data

---
+++ main.js
@@ -0,0 +1,13 @@
+import songs from './songs.js';
+
+// for (let song of songs) {
+// 	if (song.cover) {
+// 		var blob = new Blob([new Uint8Array(song.cover.data)], {
+// 			type: song.cover.format,
+// 		});
+// 		var url = URL.createObjectURL(blob);
+// 		const img = document.createElement('img');
+// 		document.body.appendChild(img);
+// 		img.setAttribute('src', url);
+// 	}
+// }
---
+++ songs.js
@@ -0,0 +1,39 @@
+const fileNames = ['1', '2', '3', '4', '5'];
+
+let songs = [];
+
+class Song {
+	constructor(fileName, title, artist, cover) {
+		this.fileName = fileName + '.mp3';
+		this.title = title;
+		this.artist = artist;
+		this.cover = cover;
+	}
+}
+
+for (let fileName of fileNames) {
+	await createSong(fileName);
+	function createSong(fileName) {
+		return new Promise(function (resolve, reject) {
+			const data = jsmediatags.read(
+				window.location.href + '/Music/' + fileName + '.mp3',
+				{
+					onSuccess: function (tag) {
+						const song = new Song(
+							fileName,
+							tag.tags.title,
+							tag.tags.artist,
+							tag.tags.picture
+						);
+						resolve(songs.push(song));
+					},
+					onError: function (error) {
+						reject(console.log(':(', error.type, error.info));
+					},
+				}
+			);
+		});
+	}
+}
+
+export default songs;
